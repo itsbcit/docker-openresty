58b22fa146283f6892d2bc325787836e51a24e2d
Jesse Weisner <jesse@weisner.ca>
2020-05-26 15:00:39 -0700

    new image based on bcit nginx

---
+++ 50-copy-config.sh
@@ -0,0 +1,27 @@
+config_path=${CONFIG_PATH:-/config}
+dest_path=${HTTPD_CONFIG_PATH:-/usr/local/openresty/nginx/conf}
+
+destfilename() {
+    sourcefile=$1
+    prefix=$2
+
+    echo $(echo $(basename $sourcefile) | sed "s/^$prefix-//")
+}
+
+if [ -d $config_path ]; then
+    for f in $(find -L ${config_path} -maxdepth 1 -type f -name "*.conf");do
+        case $(basename $f) in
+            conf.d-*.conf)
+                [ -d $dest_path/conf.d ] || mkdir -pv $dest_path/conf.d
+                cp -fv $f $dest_path/conf.d/$(destfilename $f "conf.d")
+                ;;
+            vhost.d-*.conf)
+                [ -d $dest_path/vhost.d ] || mkdir -pv $dest_path/vhost.d
+                cp -fv $f $dest_path/vhost.d/$(destfilename $f "vhost.d")
+                ;;
+            *)
+                cp -fv $f ${dest_path}/
+                ;;
+        esac
+    done
+fi
